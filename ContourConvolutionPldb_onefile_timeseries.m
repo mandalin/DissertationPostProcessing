9df39b3ce3ea78ffe0316127f0121440eca9663c
mandalin <Amanda.Blair.Lind@gmail.com>
2017-03-05 03:22:44 -0500

    Fixed an egregious issue with the input waveform in ContourConvolutionPldb_onefile.m , created ContourConvolutionPldb_onefile_timeseries.m to plot timeseries from the microphones

--- ContourConvolutionPldb_onefile_timeseries.m
+++ ContourConvolutionPldb_onefile_timeseries.m
@@ -25,9 +25,9 @@ PLdBCalcFolder='/Users/mandalin/Desktop/Sort Me Now/DissertationPostProcessing';
 cd('/Users/mandalin/Desktop/Sort Me Now/Dissertation_PLdB_PostProcessing/Inputs')
 truncate_at=500;
 load('Channel_188.mat')
-%input = X_desired_fs/2;
+input = X_desired_fs/2;
 
-%We need to do our own upsampling.... the upsampling above is wrong. 
+%% %We need to do our own upsampling....
 input_voltage_fs25600=Channel_188_Voltage;
 input_pressure_fs25600=input_voltage_fs25600 / scaling_V_per_Pa;
 
@@ -35,7 +35,7 @@ input_pressure_fs25600x15 = interp(double(input_pressure_fs25600),15);
 input_pressure_fs25600x15div8 = decimate(input_pressure_fs25600x15,8);
 input=input_pressure_fs25600x15div8/2; %this should be sampled at 48khz
 
-
+%%
   c=clock;
   hr_start=c(4)
   min_start=c(5)
@@ -86,26 +86,28 @@ inputnum=1;
 Inputlisting(inputnum).name='Mic188';
 
 %Creat text file for outputing positions and PLdB Values
-cd(ContourOutFolder)
- OutputPLdB_File_fid=fopen('PLdB_Contour_Wall_2.txt','w')
- OutputPmax_File_fid=fopen('Pmax_Contour_Wall_2.txt','w')
- OutputNumRefs_File_fid=fopen('NumRefs_Contour_Wall_2.txt','w')
-  
-  fprintf(OutputPLdB_File_fid, '%s\n', IRlisting(1).name);
-  fprintf(OutputPLdB_File_fid, '%s\n', Inputlisting(inputnum).name);
-  fprintf(OutputPLdB_File_fid, '%s\t%s\t%s\t%s\t%s\n', 'irnum', 'xpos', 'ypos', 'zpos', 'PLdB');
-   
-  fprintf(OutputPmax_File_fid, '%s\n', IRlisting(1).name);
-  fprintf(OutputPmax_File_fid, '%s\n', Inputlisting(inputnum).name);
-  fprintf(OutputPmax_File_fid, '%s\t%s\t%s\t%s\t%s\n', 'irnum', 'xpos', 'ypos', 'zpos', 'PLdB');
-   
-  fprintf(OutputNumRefs_File_fid, '%s\n', IRlisting(1).name);
-  fprintf(OutputNumRefs_File_fid, '%s\n', Inputlisting(inputnum).name);
-  fprintf(OutputNumRefs_File_fid, '%s\t%s\t%s\t%s\t%s\n', 'irnum', 'xpos', 'ypos', 'zpos', 'PLdB');
+% cd(ContourOutFolder)
+%  OutputPLdB_File_fid=fopen('PLdB_Contour_Wall_2.txt','w')
+%  OutputPmax_File_fid=fopen('Pmax_Contour_Wall_2.txt','w')
+%  OutputNumRefs_File_fid=fopen('NumRefs_Contour_Wall_2.txt','w')
+%   
+%   fprintf(OutputPLdB_File_fid, '%s\n', IRlisting(1).name);
+%   fprintf(OutputPLdB_File_fid, '%s\n', Inputlisting(inputnum).name);
+%   fprintf(OutputPLdB_File_fid, '%s\t%s\t%s\t%s\t%s\n', 'irnum', 'xpos', 'ypos', 'zpos', 'PLdB');
+%    
+%   fprintf(OutputPmax_File_fid, '%s\n', IRlisting(1).name);
+%   fprintf(OutputPmax_File_fid, '%s\n', Inputlisting(inputnum).name);
+%   fprintf(OutputPmax_File_fid, '%s\t%s\t%s\t%s\t%s\n', 'irnum', 'xpos', 'ypos', 'zpos', 'PLdB');
+%    
+%   fprintf(OutputNumRefs_File_fid, '%s\n', IRlisting(1).name);
+%   fprintf(OutputNumRefs_File_fid, '%s\n', Inputlisting(inputnum).name);
+%   fprintf(OutputNumRefs_File_fid, '%s\t%s\t%s\t%s\t%s\n', 'irnum', 'xpos', 'ypos', 'zpos', 'PLdB');
 
 last=length(IRlisting)
 
 IRnum=1;
+
+last=7; %this is how many microphones there are
 
 for(IRnum=1:last)
     
@@ -125,10 +127,11 @@ for(IRnum=1:last)
     Y=conv(IR(1:truncate_at),input);
     Y=[zeros(fs*3,1); Y]; 
     PLdBofY(IRnum)=PLdB(double(Y),FS);
-     
     PmaxofY(IRnum)=max(Y);
     NumRefs(IRnum)=sum(IR);
-     
+    Ystored{IRnum}=Y;
+    FSstored=FS;
+    IRstored{IRnum}=IR(1:truncate_at);
      %parse_the_input_file_name
      [a,b,c,d,e,f,g,h,i,j,k,l,m,n] = strread(IRlisting(IRnum).name,'%s%s%s%s%s%s%s%s%s%s%s%s%s%s','delimiter','_');
   
@@ -138,10 +141,10 @@ for(IRnum=1:last)
      [tempz,trash,trash1]= strread(n{1},'%s%s%s','delimiter','k');
       
         
-     fprintf(OutputPLdB_File_fid, '%s\t%s\t%s\t%s\t%f\n',e{1} , tempx{1}, tempy{1}, tempz{1}, PLdBofY(IRnum));
-     fprintf(OutputPmax_File_fid, '%s\t%s\t%s\t%s\t%f\n',e{1} , tempx{1}, tempy{1}, tempz{1}, PmaxofY(IRnum));
-     fprintf(OutputNumRefs_File_fid, '%s\t%s\t%s\t%s\t%f\n',e{1} , tempx{1}, tempy{1}, tempz{1}, NumRefs(IRnum));
-            
+%      fprintf(OutputPLdB_File_fid, '%s\t%s\t%s\t%s\t%f\n',e{1} , tempx{1}, tempy{1}, tempz{1}, PLdBofY(IRnum));
+%      fprintf(OutputPmax_File_fid, '%s\t%s\t%s\t%s\t%f\n',e{1} , tempx{1}, tempy{1}, tempz{1}, PmaxofY(IRnum));
+%      fprintf(OutputNumRefs_File_fid, '%s\t%s\t%s\t%s\t%f\n',e{1} , tempx{1}, tempy{1}, tempz{1}, NumRefs(IRnum));
+%             
       x{IRnum}= tempx{1};
       y{IRnum}= tempy{1};
       z{IRnum}= tempz{1};
@@ -153,9 +156,9 @@ end
   min_complete=c(5)
    
 
-  fclose(OutputNumRefs_File_fid)
-  fclose(OutputPmax_File_fid)
-  fclose(OutputPLdB_File_fid)
+%   fclose(OutputNumRefs_File_fid)
+%   fclose(OutputPmax_File_fid)
+%   fclose(OutputPLdB_File_fid)
 
   c=clock;
   hr_complete=c(4)
@@ -166,4 +169,23 @@ end
 
 hr_complete-hr_start
 min_complete-min_start
+%% Plot the Input Waveform
+IRnum=1;
+
+delta_t=1/FS;
+time_input=([1:length(input)]-1)*delta_t;
+plot(time_input,input)
+%% Plot the IR
+IRnum=1;
+
+delta_t=1/FS;
+time_IR=([1:length(IRstored{IRnum})]-1)*delta_t;
+plot(time_IR,IRstored{IRnum})
+%% Plot the Convolved Waveform
+mic_num=1; 
+
+delta_t=1/FS;
+time=([1:length(Ystored{mic_num})]-1)*delta_t;
+plot(time,Ystored{mic_num})
+
 

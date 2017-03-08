1f5327b14746c41571b7c6a3a40628106bd848b8
mandalin <Amanda.Blair.Lind@gmail.com>
2017-03-08 00:28:43 -0500

    L shaped building contour creation, the last 10 thousand IRS

--- ContourConvolutionPldb_onefile.m
+++ ContourConvolutionPldb_onefile.m
@@ -8,15 +8,17 @@ close all
 clc
 
 
-
+L=1;
+Wall=0;
 
 %identify the IR folder
 %IRFolder='/Volumes/AMPULINA/2016NovemberSims/Wall - 2016 - Nov - 25 /Specular_IR'; 
-IRFolder='/Volumes/AMPULINA/2017/Wall/GroundLevel/SpecularIR/1 commit 66346a3a92d739d2ea3df131637dc61b8273eb49'; 
+IRFolder='/Volumes/AMPULINA/2017/L/ListenerLevel/SpecularIR/1'; 
+
 cd(IRFolder)
 IRlisting=dir();
 
-ContourOutFolder='/Volumes/AMPULINA/2017/Wall/GroundLevel/SpecularContour/1';
+ContourOutFolder='/Volumes/AMPULINA/2017/L/ListenerLevel/SpecularContour/1';
 OneStepBack='/Volumes/AMPULINA/2017/Wall/GroundLevel/SpecularContour';
 PLdBCalcFolder='/Users/mandalin/Desktop/Sort Me Now/DissertationPostProcessing';
 
@@ -33,9 +35,24 @@ input_pressure_fs25600=input_voltage_fs25600 / scaling_V_per_Pa;
 
 input_pressure_fs25600x15 = interp(double(input_pressure_fs25600),15); 
 input_pressure_fs25600x15div8 = decimate(input_pressure_fs25600x15,8);
-input=input_pressure_fs25600x15div8/2; %this should be sampled at 48khz
+input=input_pressure_fs25600x15div8/2; %this should be sampled at 48khz, division by two compensates for ground reflection
 
-
+% Plot of the input waveform 
+% FS=48000;
+% delta_t=1/FS;
+% time=([1:length(input)]-1)*delta_t;
+% plot(time,input,'LineWidth',2);
+% xlabel('Time [s]','FontSize',16);
+% ylabel('Pressure [Pa]','FontSize',16);
+% axis tight
+% ylim([min(input)*1.1,max(input)*1.1])
+% xlim([.5,4.5])
+% title({'Input Waveform: Captured at microphone 188,'; 'shown scaled by .5 '; 'Pmax=35.6Pa PLdB=92.6'},'FontSize',16)
+% 
+% max(input)
+% 
+% PLdBofinput=PLdB(double(input),FS);
+%
   c=clock;
   hr_start=c(4)
   min_start=c(5)
@@ -107,7 +124,7 @@ last=length(IRlisting)
 
 IRnum=1;
 
-for(IRnum=1:last)
+for(IRnum=19038:last)
     
     cd(IRFolder)
     fid = fopen(IRlisting(IRnum).name);
@@ -130,9 +147,13 @@ for(IRnum=1:last)
     NumRefs(IRnum)=sum(IR);
      
      %parse_the_input_file_name
+     if(L)
+     [b,c,d,e,f,g,h,i,j,k,l,m,n] = strread(IRlisting(IRnum).name,'%s%s%s%s%s%s%s%s%s%s%s%s%s','delimiter','_');
+     end
+     
+     if(Wall)
      [a,b,c,d,e,f,g,h,i,j,k,l,m,n] = strread(IRlisting(IRnum).name,'%s%s%s%s%s%s%s%s%s%s%s%s%s%s','delimiter','_');
-  
-  
+     end
      [tempx,trash]= strread(l{1},'%s%s','delimiter','i')
      [tempy,trash]= strread(m{1},'%s%s','delimiter','j');
      [tempz,trash,trash1]= strread(n{1},'%s%s%s','delimiter','k');
---
+++ longmeas1.pl
@@ -0,0 +1,22 @@
+  
+  Calibration. D.C. offset is    0.000000      Pascals
+  
+  Average Initial value is    0.000000      Pascals
+  
+        Raw MAX. and MIN. are    70.23048      -55.21907      Pascals
+  
+  FrontEnd Corrected MAX. and MIN. are    70.23048      -55.21907      Pascals
+  FrontEnd Corrected MAX. and MIN. are    1.466802      -1.153281      psf
+  FrontEnd Corrected MAX. and MIN. are    130.9099       128.8212      dB
+  
+  FEC pressures (psf) at start,                  first knee,last knee and end, all nominal times
+   0.000000       0.000000       0.000000       0.000000    
+ Threshold weighted level =    84.17782      dB
+ Loudness of wave  =    98.11129      dB(PL)
+ Loudness of frontal wave  =    107.0138      dB(Z)
+ Loudness of diffuse wave  =    107.2521      dB(Z)
+ Noisiness of wave  =    105.4680      dB(PNL)
+ Noisiness of wave  =    89.03482      dB(A) (1/3 ob)
+ Level of wave  =    105.5313      dBC
+ Level of wave  =    80.62640      dBA
+ Level of wave  =    116.5165      dBLin
